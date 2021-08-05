1502ca137c675ac03ba26b191acf2a30ac7dca04
ihValery <ihValery@gmail.com>
2021-08-05 16:51:10 +0300

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 05.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
