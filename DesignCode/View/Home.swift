7e0dbdf502adbcef286ec46e63032064ecdbc7df
ihValery <ihValery@gmail.com>
2021-08-07 22:23:12 +0300

    SubPlus_13. ScrollView and Repeat

--- DesignCode/View/Home.swift
+++ DesignCode/View/Home.swift
@@ -19,38 +19,7 @@ struct Home: View {
                 Color.background2
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack {
-                    HStack {
-                        Text("Watching")
-                            .font(.system(size: 28, weight: .bold))
-                        Spacer()
-                        
-                        Button(action: {
-                            showProfile.toggle()
-                        }, label: {
-                            Image("Avatar")
-                                .renderingMode(.original) //Уже не обязтелен!
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 36, height: 36)
-                                .clipShape(Circle())
-                        })
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 30)
-                    
-                    Spacer()
-                }
-                .padding(.top, screen.height < 750 ? 0 : 44)
-                .background(Color.white)
-                .clipShape(RoundedRectangle(cornerRadius: showProfile ? 30 : 0, style: .continuous))
-                .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
-                .rotation3DEffect(.degrees(showProfile ? Double(profileState.height / 10) - 10 : 0),
-                                  axis: (x: 10, y: 0, z: profileState.width / 10))
-                .offset(y: showProfile ? profileState.height - 400 : 0)
-                .scaleEffect(showProfile ? 0.9 : 1)
-                .animation(.spring(response: 0.5, dampingFraction: 0.65, blendDuration: 0))
-                .edgesIgnoringSafeArea(.all)
+                HomeView(showProfile: $showProfile, profileState: $profileState)
             }
             .onTapGesture {
                 showProfile = false
@@ -59,7 +28,7 @@ struct Home: View {
             ProfileMenuView()
                 .offset(y: showProfile ? 0 : screen.height)
                 .offset(x: profileState.width, y: profileState.height)
-                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .animation(.spring(response: 0.5, dampingFraction: 0.6))
                 .gesture(DragGesture()
                             .onChanged { value in
                                 if profileState.height > 70 {
---
+++ DesignCode/View/SubView/HomeView.swift
@@ -0,0 +1,59 @@
+//
+//  HomeView.swift
+//  DesignCode
+//
+//  Created by Валерий Игнатьев on 07.08.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var screen = ScreenBounds()
+    
+    @Binding var showProfile: Bool
+    @Binding var profileState: CGSize
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Watching")
+                    .font(.system(size: 28, weight: .bold))
+                Spacer()
+                
+                AvatarButton(showProfile: $showProfile)
+            }
+            .padding(.horizontal)
+            .padding(.leading, 16)
+            .padding(.top, 30)
+                
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
+            
+            Spacer()
+        }
+        .padding(.top, screen.height < 750 ? 0 : 44)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: showProfile ? 30 : 0, style: .continuous))
+        .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
+        .rotation3DEffect(.degrees(showProfile ? Double(profileState.height / 10) - 10 : 0),
+                          axis: (x: 10, y: 0, z: profileState.width / 10))
+        .offset(y: showProfile ? profileState.height - 400 : 0)
+        .scaleEffect(showProfile ? 0.9 : 1)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(showProfile: .constant(false), profileState: .constant(.zero))
+            .previewDevice("iPhone 12 Pro")
+    }
+}
